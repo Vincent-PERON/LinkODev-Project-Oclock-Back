5a0364e814f7707d5b60aabd2a46efe8586bcad6
Alexandre JOUANY <alexandre.jouany@gmail.com>
2022-09-30 13:52:56 +0200

    feat: add authcontroller.js

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,3 +1,4 @@
+const { User } = require("../models");
 
 
 module.exports = { 
@@ -16,7 +17,33 @@ module.exports = {
      * @param {*} res HTTP response from Express app
     */
     async doLogin(req,res){
-        res.json("doLogin");
+        try {
+            // find user
+            console.log(req.body)
+            const foundUser = await User.findOne(
+                {
+                    where : {
+                        email: req.body.email
+                    }
+                }
+            );
+            
+            // if user not found, send a message
+            if (!foundUser) {
+                return res.status(401).json({error: "Email or password is incorrect."});
+            }
+
+            // user exists, test the password
+            const validPassword = foundUser.password === req.body.password; // TODO : use bcrypt or similar
+
+            // if password incorrect, send a message
+            if (!validPassword) {
+                return res.status(401).json({error: "Email or password is incorrect."});
+            }
+            res.json(foundUser);
+        } catch (error) {
+            res.status(500).json({error: "Internal Server Error (Login)"});
+        }
     },
 
     /**
